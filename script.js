33c3402cf39cf2a1eb4329538b1eca0251736925
Looserty <115899596+looserty1@users.noreply.github.com>
2022-11-24 16:12:33 +0000

    When the KeyS is kick

--- script.js
+++ script.js
@@ -5,43 +5,67 @@ c.width = c.height;
 const pxsize = c.height / 128;
 let key;
 const fps = 15;
-
+const idle = document.getElementById("idle");
+const kick = document.getElementById("img"); //Right
+const kick1 = document.getElementById("img1"); //Leftf
 //[frame, x, y, width, height]
 
-let FBI = ["frame", 0, 0, 10, 20];
+let FBI = [idle, 0, 0, 10, 20];
 
 function spriter(character) {
   ctx.fillRect(character[1] * pxsize, character[2] * pxsize, character[3] * pxsize, character[4] * pxsize);
+}
+function spritere(character) {
+  ctx.drawImage(character[0], character[1] * pxsize, character[2] * pxsize, character[3] * pxsize, character[4] * pxsize);
 }
 let yv = 0;
 let xv = 0;
 let number = 0;
 function onupdate() {
   ctx.clearRect(0, 0, c.width, c.height);
-  spriter(FBI);
+  spritere(FBI);
   //Game Play
-  if (FBI[2] < 108) {
+  if (FBI[2] < 128 - FBI[4]) {
     yv += 1;
     ground = false;
   } else {
     yv = 0;
-    FBI[2] = 108;
+    FBI[2] = 128 - FBI[4];
     ground = true;
+  }
+  if (map[translate("KeyS")] && xv != 0) {
+    if (xv != 0) {
+      xv += 2 * Math.abs(xv) / xv;
+    }
+    if (xv > 0) {
+      FBI[0] = kick;
+    } else {
+      FBI[0] = kick1;
+    }
+    FBI[3] = 20;
+    FBI[4] = 10;
+    if (Math.abs(xv) > 15) {
+      xv = 15 * Math.abs(xv)/xv;
+    }
+  } else {
+    FBI[0] = idle;
+    FBI[3] = 10;
+    FBI[4] = 20;
   }
   if (map[translate("KeyW")] && ground) {
     yv = -6;
   }
-  if (map[translate("KeyD")] && FBI[1] < 108) {
+  if (map[translate("KeyD")] && FBI[1] < 128 - FBI[4]) {
     xv += 1;
   }
   if (map[translate("KeyA")]) {
     xv -= 1;
   }
   if (!(map[translate("KeyD")]) && !(map[translate("KeyA")]) && xv != 0) {
-    xv += Math.abs(xv)/xv * -1;
+    xv += Math.abs(xv) / xv * -1;
   }
-  if (Math.abs(xv) > 10) {
-    xv = Math.abs(xv)/xv * 10;
+  if (Math.abs(xv) > 10 && !map[translate("KeyS")]) {
+    xv = Math.abs(xv) / xv * 10;
   }
   number++;
   FBI[1] += xv;
@@ -70,16 +94,16 @@ function translate(nm) {
   text = text.split("");
   let i = -1;
   let shift = 0;
-  if  (Number.isInteger(shift)) {} else {
+  if (Number.isInteger(shift)) { } else {
     shift = 0;
   }
   let a2 = [];
-  while (i<text.length-1) {
+  while (i < text.length - 1) {
     i += 1;
-    if (a.indexOf(text[i])==-1) {
+    if (a.indexOf(text[i]) == -1) {
       a2.push(0)
     } else {
-      a2.push(a.indexOf(text[i])+shift)
+      a2.push(a.indexOf(text[i]) + shift)
     }
   }
   return parseInt(a2.join(""));
